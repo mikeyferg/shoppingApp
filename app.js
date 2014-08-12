3508d144cfd8ba53b0140f871abcc0488532c3f4
mikey <mikey@mikeys-MacBook-Pro.local>
2014-08-12 13:22:25 -0700

    adding some jQuery

---
+++ app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('#add').on('click', 'button', function() {
+		/*alert("hello"); */
+		var newItem = $("<div></div>");
+
+		$('.sections').append('<th>test</th>');
+	})
+
+
+
+
+})
