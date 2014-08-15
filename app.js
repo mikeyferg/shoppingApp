4dda7e9ff5debc537214fd9fc6971c8825f5e000
mikey <mikey@mikeys-MacBook-Pro.local>
2014-08-15 13:01:31 -0700

    total price now working

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	
-	$('#add').on('click', 'button', function(e) {
+	$('#add').on('click submit', 'button', function(e) {
 		
 		/* prevent clearing on new element added */
 		e.preventDefault();
@@ -9,8 +9,9 @@ $(document).ready(function() {
 		var newItem = $(this).closest("#add").find(".itemUser").val();
 		
 		/*create new elements w/ user inputs */
-		
 		var row = $('<tr></tr>');
+
+		var item = $(row).addClass(newItem);
 
 		var status = $('<td></td>', {
 			class: "status"
@@ -43,4 +44,56 @@ $(document).ready(function() {
 		/* clear user input */
 		$('input').val('');
 	})
+
+	/* store usr input values for quantity, price and total inputs */
+	$('input').on('keyup', function() {
+		var value = $(this).val();
+		$(this).text(value);
+
+		/* calcuate total cost for a row */
+		if ( $(this).closest('tr').find('.quantity').text().length > 0 && $(this).closest('tr').find('.value').text().length > 0) {
+
+			var price = +$(this).closest('tr').find('.value').text();
+			var quantity = +$(this).closest('tr').find('.quantity').text();
+
+			$(this).closest('td').siblings('.total').text(price * quantity);
+			
+		}
+		/* calculate total costs */
+    	var sum = 0;
+    
+    	for (var i=0; i < $('.total').length; i++ ){
+    		/*sum = sum + parseInt($($('.total')[i]).text(), 10); */
+    		/*sum = sum + 1; */
+    		if (parseInt(parseInt($($('.total')[i]).text()))) {
+    			sum = sum + parseInt($($('.total')[i]).text());
+    		}
+    	}
+    	
+    	$('#total p').text(sum);
+
+    	
+	})
+
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
